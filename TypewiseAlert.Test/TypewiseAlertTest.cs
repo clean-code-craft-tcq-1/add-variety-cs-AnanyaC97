741264af604995b93a0852cce1849916ebd0237a
Ananya Chandramohan <77439415+AnanyaC97@users.noreply.github.com>
2021-04-01 19:53:53 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -10,14 +10,30 @@ namespace TypewiseAlert.Test
         public void InfersBreachAsPerLimits()
         {
             Assert.True(TypewiseAlert.InferBreach(12, 20, 30) == BreachType.TOO_LOW);
+        }
+        [Fact]
+        public void InfersBreachAsPerLimits()
+        {
             Assert.True(TypewiseAlert.InferBreach(35, 20, 30) == BreachType.TOO_HIGH);
+        }
+        [Fact]
+        public void InfersBreachAsPerLimits()
+        {
             Assert.True(TypewiseAlert.InferBreach(25, 20, 30) == BreachType.NORMAL);
         }
         [Fact]
         public void ClassifyTemperatureAsPerLimits()
         {
             Assert.True(TypewiseAlert.classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, -1) == BreachType.TOO_LOW);
+        }
+        [Fact]
+        public void ClassifyTemperatureAsPerLimits()
+        {
             Assert.True(TypewiseAlert.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 10) == BreachType.NORMAL);
+        }
+        [Fact]
+        public void ClassifyTemperatureAsPerLimits()
+        {
             Assert.True(TypewiseAlert.classifyTemperatureBreach(CoolingType.PASSIVE_COOLING, 40) == BreachType.TOO_HIGH);
         }
         [Fact]
