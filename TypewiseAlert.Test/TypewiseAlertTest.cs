50db2cfd73dac844de2aeb2a040faadc54ffbf82
Ananya Chandramohan <77439415+AnanyaC97@users.noreply.github.com>
2021-04-01 19:14:49 +0530

    Add files via upload

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -1,15 +1,32 @@
 using System;
 using Xunit;
+using static TypewiseAlert.TypewiseConstants;
 
 namespace TypewiseAlert.Test
 {
-  public class TypewiseAlertTest
-  {
-    [Fact]
-    public void InfersBreachAsPerLimits()
+    public class TypewiseAlertTest
     {
-      Assert.True(TypewiseAlert.inferBreach(12, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
+        [Fact]
+        public void InfersBreachAsPerLimits()
+        {
+            Assert.True(TypewiseAlert.InferBreach(12, 20, 30) == BreachType.TOO_LOW);
+            Assert.True(TypewiseAlert.InferBreach(35, 20, 30) == BreachType.TOO_HIGH);
+            Assert.True(TypewiseAlert.InferBreach(25, 20, 30) == BreachType.NORMAL);
+        }
+        [Fact]
+        public void ClassifyTemperatureAsPerLimits()
+        {
+            Assert.True(TypewiseAlert.classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, -1) == BreachType.TOO_LOW);
+            Assert.True(TypewiseAlert.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 10) == BreachType.NORMAL);
+            Assert.True(TypewiseAlert.classifyTemperatureBreach(CoolingType.PASSIVE_COOLING, 40) == BreachType.TOO_HIGH);
+        }
+        [Fact]
+        public void CheckAlertAsPerLimits()
+        {
+            BatteryCharacter batteryCharacter;
+            batteryCharacter.coolingType = CoolingType.MED_ACTIVE_COOLING;
+            batteryCharacter.brand = "ETAS";
+            TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, batteryCharacter, 20);
+        }
     }
-  }
 }
